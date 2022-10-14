93587e54d1895b449a75652de09f3fb54d86857e
Remukan Astor <82083002+remukanastor@users.noreply.github.com>
2022-10-14 11:22:24 +0700

    edit warna tombol

--- script/main.js
+++ script/main.js
@@ -122,7 +122,7 @@ const animationTimeline = () => {
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
       x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
+      backgroundColor: "#CD29B3",
       color: "#fff",
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
